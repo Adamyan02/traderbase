5a92f1fb3b0fe9ca5d106dec3a6b01b328758c9a
Adamyan02 <adamsosyastr@yandex.ru>
2026-06-11 16:18:10 +0400

    Delete app/ exchanges directory

--- app/ exchanges/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ExchangesPage() {
-  return (
-    <main className="min-h-screen bg-slate-950 text-white p-10">
-      <h1 className="text-3xl font-bold">Биржи</h1>
-    </main>
-  )
-}
