e247ed44137f316c906aafd4dcbd1582d8b6ef9f
Adamyan02 <adamsosyastr@yandex.ru>
2026-06-11 12:40:28 +0400

    Update page.tsx

--- app/ exchanges/page.tsx
+++ app/ exchanges/page.tsx
@@ -1,1 +1,7 @@
-
+export default function ExchangesPage() {
+  return (
+    <main className="min-h-screen bg-slate-950 text-white p-10">
+      <h1 className="text-3xl font-bold">Биржи</h1>
+    </main>
+  )
+}
