759624264dffff3e814ae5b5107445e83a2ccaf8
Adamyan02 <adamsosyastr@yandex.ru>
2026-06-11 16:31:27 +0400

    Update navigation.ts

--- data/navigation.ts
+++ data/navigation.ts
@@ -1,1 +1,9 @@
-
+export const navigation = [
+  { href: "/", label: "Главная" },
+  { href: "/learning", label: "Обучение" },
+  { href: "/practice", label: "Практика" },
+  { href: "/blog", label: "Блог" },
+  { href: "/exchanges", label: "Биржи" },
+  { href: "/finance", label: "Финансы" },
+  { href: "/about", label: "О проекте" },
+]
