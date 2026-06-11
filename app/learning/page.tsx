4ee34f23bc64f64989de73011d48d83c23a70a5e
Adamyan02 <adamsosyastr@yandex.ru>
2026-06-11 12:39:29 +0400

    Update page.tsx

--- app/learning/page.tsx
+++ app/learning/page.tsx
@@ -1,1 +1,7 @@
-
+export default function LearningPage() {
+  return (
+    <main className="min-h-screen bg-slate-950 text-white p-10">
+      <h1 className="text-3xl font-bold">Обучение</h1>
+    </main>
+  )
+}
