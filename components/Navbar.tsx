d5d490918efe40289ae32ee9c7a6bf5315f1e317
Adamyan02 <adamsosyastr@yandex.ru>
2026-06-11 16:33:14 +0400

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { navigation } from "@/data/navigation"
 
 export default function Navbar() {
   const pathname = usePathname()
