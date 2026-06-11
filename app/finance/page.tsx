211f498e9a3780ba70f453e1e873b828659bf51b
Adamyan02 <adamsosyastr@yandex.ru>
2026-06-11 12:41:04 +0400

    Update page.tsx

--- app/finance/page.tsx
+++ app/finance/page.tsx
@@ -1,1 +1,7 @@
-
+export default function FinancePage() {
+  return (
+    <main className="min-h-screen bg-slate-950 text-white p-10">
+      <h1 className="text-3xl font-bold">Финансы</h1>
+    </main>
+  )
+}
